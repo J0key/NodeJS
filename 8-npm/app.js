6f962ccf612f9d63ad75edf2bae4061ec86ddae4
J0key <shyraathaya@mail.ugm.ac.id>
2024-08-18 16:48:49 +0700

    feat: chalk

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -2,5 +2,5 @@ const chalk = require('chalk');
 const validator = require('validator');
 
 const nama = 'shyra';
-const pesan = chalk`halo perkenalkan nama saya {bgRed ${nama}}`;
+const pesan = chalk`{bgBlue halo} perkenalkan nama saya {bgRed ${nama}}`;
 console.log(pesan);
