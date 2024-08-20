a735bff09b454a8f28fdd41881bf503f0c4fefc1
J0key <shyraathaya@mail.ugm.ac.id>
2024-08-20 11:05:18 +0700

    feat: 11

--- Contact-App/app.js
+++ Contact-App/app.js
@@ -1,17 +1,42 @@
-const { pertanyaan, simpanKontak } = require('./contacts');
-
-const daftarKontak = async() => {
-    const nama = await pertanyaan('masukkan nama anda: ')
-    const umur = await pertanyaan('masukkan umur anda: ')
-
-    simpanKontak( nama , umur);
+const {  simpanKontak } = require('./contacts');
+const yargs = require('yargs');
 
 
-}
+// const daftarKontak = async() => {
+//     const nama = await pertanyaan('masukkan nama anda: ')
+//     const umur = await pertanyaan('masukkan umur anda: ')
 
-daftarKontak();
+//     simpanKontak( nama , umur);
 
 
+// }
+
+// daftarKontak();
 
 
+yargs.command({
+    command: 'add',
+    describe: 'Tambahkan kontak baru',
+    builder: {
+        nama: {
+            describe: 'Masukkan nama kontak',
+            type:'string',
+            demandOption: true
+        },
+        email: {
+            describe: 'Masukkan email kontak',
+            type:'email',
+            demandOption: true
+        },
+        umur: {
+            describe: 'Masukkan umur kontak',
+            type: 'number',
+            demandOption: true
+        }
+    },
+    handler(argv) {
+        simpanKontak(argv.nama, argv.email, argv.umur);
+    }
+})
 
+yargs.parse()
--- Contact-App/contacts.js
+++ Contact-App/contacts.js
@@ -1,52 +1,77 @@
 const { dir } = require('console');
 const fs = require('fs');
 const readline = require('readline');
+const chalk = require('chalk');
+const validator = require('validator');
 
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
 
 //Membuat folder data
 const dirPath = './data'
-if(!fs.existsSync(dirPath)){
+if (!fs.existsSync(dirPath)) {
     fs.mkdirSync(dirPath);
 }
 
-if(!fs.existsSync('./data/contacts.json')){
-    fs.writeFileSync('./data/contacts.json', '[]' , 'utf-8');
+if (!fs.existsSync('./data/contacts.json')) {
+    fs.writeFileSync('./data/contacts.json', 'utf-8');
 }
 
 
-const pertanyaan = (pertanyaan) => {
-    return new Promise((resolve) => {
-        rl.question(pertanyaan, (answer) => {
-            resolve(answer); 
-        });
-    });
-}
+// const pertanyaan = (pertanyaan) => {
+//     return new Promise((resolve) => {
+//         rl.question(pertanyaan, (answer) => {
+//             resolve(answer); 
+//         });
+//     });
+// }
 
-const simpanKontak = (nama ,umur)=>{
-    const contact = {nama ,umur};
-    const file = fs.readFileSync('data/contacts.json' , 'utf-8');
+const simpanKontak = (nama, email, umur) => {
+    const contact = { nama, email, umur };
+    const file = fs.readFileSync('data/contacts.json', 'utf-8');
 
-    try{
+    try {
         // kalo file nya ga kososng, isinya di parsing ke JSON, kalau kosong dikasi array kosong
-        const contacts = JSON.parse(file);
+        const contacts = file.length ? JSON.parse(file) : [];
+
+        // cek duplikat
+        const duplikat = contacts.find((contact) => contact.nama == nama)
+        if (duplikat) {
+            console.log(chalk.inverse.red.bold('Nama sudah ada, silahkan masukkan nama yang lain.'));
+            return false;
+        }
+
+        // cek email
+        if (!validator.isEmail(email)) {
+            console.log(chalk.inverse.red.bold('Format email salah, silahkan masukkan email yang valid.'));
+            return false;
+        }
+
+        // cek HP
+        if (!validator.isNumeric(umur)) {
+            console.log(chalk.inverse.red.bold('Format email salah, silahkan masukkan email yang valid.'));
+            return false;
+        }
+
+
+
 
         contacts.push(contact);
-        
-        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts ,null , 2));
+
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts, null, 2));
 
         console.log(`Selamat datang, ${nama}!`);
 
-    } catch(err){
+    } catch (err) {
         console.log('Error reading file:', err);
         return;
     }
-    rl.close()
+    // rl.close()
 }
 
-module.exports = { simpanKontak, pertanyaan};
+module.exports = { simpanKontak };
